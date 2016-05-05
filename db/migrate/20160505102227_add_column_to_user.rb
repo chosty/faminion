d0fcf33c4f047a68ee98c941a1936a6f50fe7c78
chosty <tyosuthi@gmail.com>
2016-05-05 19:37:28 +0900

    userテーブル作成

--- db/migrate/20160505102227_add_column_to_user.rb
+++ db/migrate/20160505102227_add_column_to_user.rb
@@ -1,4 +1,11 @@
 class AddColumnToUser < ActiveRecord::Migration
   def change
+    add_column :users, :family_id, :integer
+    add_column :users, :name, :string
+    add_column :users, :birthday, :date
+    add_column :users, :gender, :string
+    add_column :users, :role, :string
+
+    add_index :users, :family_id
   end
 end
