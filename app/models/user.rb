d9b4f5675acfab5fbda92d60f3e3a5fc2458cf75
chosty <tyosuthi@gmail.com>
2016-05-06 21:36:32 +0900

    userモデルのgenderとroleのenum定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,8 @@ class User < ActiveRecord::Base
   has_many :post_favs
   has_many :events, through: :event_target_users
 
+  enum gender: { no_select: 0, male: 1, female: 2 }
+  enum role: { no_select: 0, father: 1, mother: 2, child: 3 }
+
   scope :my_families, ->(user_id){ where(family_id: Family.where(id: User.find(user_id).family_id)) }
 end
