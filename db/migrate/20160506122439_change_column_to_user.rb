994c54c652e980b2ca4080462f560b79d7ec15e1
chosty <tyosuthi@gmail.com>
2016-05-12 01:03:06 +0900

    change_to_remove_and_add

--- db/migrate/20160506122439_change_column_to_user.rb
+++ db/migrate/20160506122439_change_column_to_user.rb
@@ -1,6 +1,7 @@
 class ChangeColumnToUser < ActiveRecord::Migration
   def change
-    change_column :users, :gender, :integer, default: 0, null: false
+    remove_column :users, :gender
+    add_column :users, :gender, :integer, default: 0, null: false
     change_column :users, :role, :integer, default: 0, null: false
   end
 end
